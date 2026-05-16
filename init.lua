3e496b1475ce76379957f23a6ebf3def2fd5544b
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-05-16 20:36:42 +0200

    Replace vim.o with vim.opt

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 -- BASIC SETTINGS
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.termguicolors = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
 
-vim.o.expandtab = true
-vim.o.shiftwidth = 2
-vim.o.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 
-vim.o.clipboard = "unnamedplus"
+vim.opt.clipboard = "unnamedplus"
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
